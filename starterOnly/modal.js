3de17073ce677ce5018c212ad3102c68884198b6
Dylan Ronne <dyroxx94@gmail.com>
2024-07-16 15:38:40 +0200

    close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,27 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbackground = document.querySelector(".bground");
+const modalButtons = document.querySelectorAll(".modal-btn");
+const formInputs = document.querySelectorAll(".formData");
+const close = document.querySelector(".close");
+const topnavbar = document.getElementById("myTopnav");
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalButtons.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+close.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbackground.style.display = "block";
 }
 
+function closeModal() {
+  modalbackground.style.display = "none";
+}
 
+function editNav() {
+topnavbar.classList.toggle("responsive");
+}
